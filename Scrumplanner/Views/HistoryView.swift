6f4ef11335daf07a4c7e8c48cd0dd892707c363b
kadirosmanust <kadirosmanust98@gmail.com>
2023-11-05 13:14:38 +0300

    feat: speech to text added for scrum history

---
+++ Scrumplanner/Views/HistoryView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    var body: some View {
+        ScrollView {
+                   VStack(alignment: .leading) {
+                       Divider()
+                                        .padding(.bottom)
+                                    Text("Attendees")
+                                        .font(.headline)
+                       HStack{
+                           ForEach(history.attendees){attendee in
+                               Text(attendee.name)
+                           }
+                       }
+                                    if let transcript = history.transcript {
+                                        Text("Transcript")
+                                            .font(.headline)
+                                            .padding(.top)
+                                        Text(transcript)
+                                    }
+                   }
+               }
+        .navigationTitle(Text(history.date, style: .date))
+           .padding()
+    }
+}
+
+
+
+struct HistoryView_Preview : PreviewProvider{
+    
+    static var history: History {
+          History(attendees: [
+              DailyScrum.Attendee(name: "Jon"),
+              DailyScrum.Attendee(name: "Darla"),
+              DailyScrum.Attendee(name: "Luis")
+          ],
+                  transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+      }
+    static var previews: some View{
+        HistoryView(history: history)
+    }
+}
